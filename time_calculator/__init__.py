d25a1cac3dfc217e9606b713134b7cfa1bce60d6
TheComputationalCore <dineshchandra962@gmail.com>
2025-11-24 02:24:51 +0530

    Update __init__.py

--- time_calculator/__init__.py
+++ time_calculator/__init__.py
@@ -1,4 +1,10 @@
 """
 Time Calculator Package
+
+This package provides the `add_time` function for performing
+12-hour time arithmetic with optional weekday handling.
 """
+
 from .time_calculator import add_time
+
+__all__ = ["add_time"]
